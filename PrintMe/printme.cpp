b2558546c240c00d78826ed89f0d8f513aff0d8a
stb098 <stb098@NE-245-C28B46ED>
2017-10-03 12:11:59 +0100

    Added a loop to be rude about your age repeatedly

--- PrintMe/printme.cpp
+++ PrintMe/printme.cpp
@@ -1,30 +1,30 @@
 // PrintMe.cpp : Defines the entry point for the console application.
 
 #include "stdafx.h"
-#include <cstring>
+
+#include <stdio.h>
+#include <tchar.h>
+#include <conio.h>
 
 #pragma warning(disable: 4996)
 
 #define TOO_OLD 23
 #define TOO_YOUNG 17
 
-struct Person
-{
+struct Person {
 	char name[30];
 	int age_years;
 	char reason_for_being_here[200];
 };
 
-// value is fine
-const char * comment_on_age(const int age) {
-	if(age >= TOO_OLD) {
-		return "Wow, you're old! Shouldn't you have a proper job?";
-	}
-	if (age <= TOO_YOUNG) {
-		return "Umm, shouldn't you finish school first?";
-	}
-	return "Wow, you're cool! Can I be your friend?";
-}
+typedef struct {
+	char name[30];
+	int age_years;
+	char reason_for_being_here[200];
+} person;
+
+void commentOnAgeWayTooMuch(const int iNoOfIterations, const char *sToPrint);
+const char * comment_on_age(const int age);
 
 int main() {
 	Person person;
@@ -33,8 +33,8 @@ int main() {
 	scanf("%30[a-zA-Z ]s\n", person.name);
 
 	printf("Enter age: ");
-	// Has to have the reference passed in as it's mutating it
-	// (if a value was passed in it could be changed, but the 'age' variable would remain the same)
+	// Has to have the address passed in as it's mutating it
+	// (if a value was passed in it could be changed, but the 'age' original variable would remain the same)
 	scanf("%d", &person.age_years);
 
 	printf("Why are you even here?\n");
@@ -43,9 +43,27 @@ int main() {
 	printf("\n-----------------------------------------\n");
 	printf("So %s, you are %d years old and you are here because:\n\"%s\"\n", person.name, person.age_years,
 	       person.reason_for_being_here);
-	printf("\n... %s", comment_on_age(person.age_years));
 
+	commentOnAgeWayTooMuch(10, comment_on_age(person.age_years));
 	_getch();
 
 	return 0;
+}
+
+void commentOnAgeWayTooMuch(const int iNoOfIterations, const char *sToPrint) {
+	printf("\n-----------------------------------------\n");
+	for (int i = 0; i < iNoOfIterations; i++) {
+		printf("%d: %s\n", i+1, sToPrint);
+	}
+}
+
+// value is fine
+const char * comment_on_age(const int age) {
+	if (age >= TOO_OLD) {
+		return "Wow, you're old! Shouldn't you have a proper job?";
+	}
+	if (age <= TOO_YOUNG) {
+		return "Umm, shouldn't you finish school first?";
+	}
+	return "Wow, you're cool! Can I be your friend?";
 }
--- PrintMe/stdafx.h
+++ PrintMe/stdafx.h
@@ -6,7 +6,3 @@
 #pragma once
 
 #include "targetver.h"
-
-#include <stdio.h>
-#include <tchar.h>
-#include <conio.h>
